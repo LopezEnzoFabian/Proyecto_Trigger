a882998a366dc206f1dddf2641977a6ff4bb33be
Enzo Fabián López <enzolopez789@gmail.com>
2023-11-20 22:40:23 -0300

    grupo 7

---
+++ Backup/Implementacion de backup.sql
@@ -0,0 +1,183 @@
+
+
+-- 1 Verificar el modo de recuperacion de la base de datos
+use base_consorcio
+
+SELECT name, recovery_model_desc
+FROM sys.databases
+WHERE name = 'base_consorcio';
+
+
+-- 2 cambiamos el modo de recuperacion
+
+USE master; -- Asegúrate de estar en el contexto de la base de datos master
+ALTER DATABASE base_consorcio
+SET RECOVERY FULL;
+
+-- 3 Realizamos backup de la base de datos
+
+BACKUP DATABASE base_consorcio
+TO DISK = 'C:\backup\consorcio_backup.bak'
+WITH FORMAT, INIT;
+
+-- Agregamos 10 registros
+
+select * from gasto;
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (1,1,1,5,GETDATE(),5,1200);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (1,2,2,5,GETDATE(),2,1630);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (3,20,2,2,GETDATE(),4,500);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (5,3,1,3,GETDATE(),3,1520);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (5,12,3,4,GETDATE(),5,1120);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (6,45,2,4,GETDATE(),4,2000);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (14,36,2,2,GETDATE(),1,1740);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (18,3,1,2,GETDATE(),2,1520);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (2,48,1,5,GETDATE(),2,1500);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (13,10,2,1,GETDATE(),1,1420);
+
+
+-- 4 Realizamos backup del log de la base de datos
+
+BACKUP LOG base_consorcio
+TO DISK = 'C:\backup\LogBackup.trn'
+WITH FORMAT, INIT;
+
+
+-- Insertamos 10 registros mas
+
+select * from gasto
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (1,1,1,5,GETDATE(),5,1200);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (2,48,1,5,GETDATE(),2,1500);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (3,16,1,2,GETDATE(),4,2300);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (4,21,1,3,GETDATE(),3,1000);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (5,3,1,2,GETDATE(),5,1500);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (6,45,2,4,GETDATE(),4,2000);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (8,17,2,2,GETDATE(),1,1300);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (9,14,1,3,GETDATE(),2,1700);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (12,7,4,2,GETDATE(),3,2100);
+
+insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
+values (13,10,2,1,GETDATE(),1,1100);
+
+
+--5 Realizamos backup del log en otra ubicacion
+
+BACKUP LOG base_consorcio
+TO DISK = 'C:\backup\logs\LogBackup2.trn'
+WITH FORMAT, INIT;
+
+--6 Restauramos el backup de la base de datos
+
+use master
+
+RESTORE DATABASE base_consorcio
+FROM DISK = 'C:\backup\consorcio_backup.bak'
+WITH REPLACE, NORECOVERY;
+
+RESTORE LOG base_consorcio
+FROM DISK = 'C:\backup\LogBackup.trn'
+WITH RECOVERY;
+
+-- Segundo log
+
+RESTORE LOG base_consorcio
+FROM DISK = 'C:\backup\logs\LogBackup2.trn'
+WITH RECOVERY;
+
+select * from gasto
+
+
+
+----- Implementacion de Transacciones y Transacciones Anidadas
+
+	
+
+
+
+
+
+
+
+----- Implementacion de Manejo de Permisos a nivel usuarios de bases de datos
+
+
+
+go
+create login manuel with password='Password123';
+create login juan with password='Password123';
+
+--Se crea los usuarios con los long in anteriores
+create user manuel for login manuel
+create user juan for login juan
+	
+--Se asignan los roles a los usuarios
+alter role db_datareader add member manuel
+alter role db_ddladmin add juan
+
+go
+-- Creamos el procedimiento insertarAdministrador
+create procedure insertarAdministrador
+@apeynom varchar(50),
+@viveahi varchar(1),
+@tel varchar(20),
+@s varchar(1),
+@nacimiento datetime
+as
+begin
+	insert into administrador(apeynom,viveahi,tel,sexo,fechnac)
+	values (@apeynom,@viveahi,@tel,@s,@nacimiento);
+end
+
+-- Probar con manuel antes del permiso 
+--insert into administrador(apeynom,viveahi,tel,sexo,fechnac) values ('Oscar Alejandro','S','391281922','M','2001-08-14')
+--exec  insertarAdministrador 'Oscar Alejandro','S','3912819222','M','2001-08-14'
+grant execute on insertarAdministrador to manuel 
+	
+-- Probar despues de el permiso 
+--exec  insertarAdministrador 'Oscar Alejandro','S','3912819222','M','2001-08-14'
+select * from administrador
+
+
+
+
+
+
+
+
---
+++ Roles-Usuarios/rolesUsuarioTarea.sql
@@ -0,0 +1,223 @@
+--
+-- triggers
+--
+create table auditoria_administrador(
+idAdministrador int,
+apeynom varchar(50),
+tel varchar(50),
+fechnac date,
+viveahi varchar(1),
+sexo varchar (1),					     
+fechaModif datetime,
+usuario_accion varchar(100),
+accion varchar (20)
+);
+go
+
+go
+CREATE TRIGGER trg_auditoria_administrador_insertar
+ON administrador
+FOR INSERT
+AS
+BEGIN
+    DECLARE
+        @idAdministrador INT,
+        @apeynom VARCHAR(50),
+        @tel VARCHAR(50),
+        @fechnac DATE,
+        @sexo VARCHAR(1),
+        @viveahi VARCHAR(1),
+        @fechaModif DATE,
+        @usuario VARCHAR(50),
+        @accion VARCHAR(20)
+
+    SELECT
+        @idAdministrador = idadmin,
+        @apeynom = ApeyNom,
+        @tel = tel,
+        @fechnac = fechnac,
+        @sexo = sexo,
+        @viveahi = viveahi,
+        @fechaModif = GETDATE(),
+        @usuario = CURRENT_USER,
+        @accion = 'accion insert'
+    FROM
+        inserted
+
+    INSERT INTO auditoria_administrador
+    VALUES (
+        @idAdministrador,
+        @apeynom,
+        @tel,
+        @fechnac,
+        @sexo,
+        @viveahi,
+        @fechaModif,
+        @usuario,
+        @accion
+    )
+END;
+
+	go
+CREATE TRIGGER trg_auditoria_administrador_modificar
+ON administrador
+FOR update
+AS
+BEGIN
+    DECLARE
+        @idAdministrador INT,
+        @apeynom VARCHAR(50),
+        @tel VARCHAR(50),
+        @fechnac DATE,
+        @sexo VARCHAR(1),
+        @viveahi VARCHAR(1),
+        @fechaModif DATE,
+        @usuario VARCHAR(50),
+        @accion VARCHAR(20)
+
+    SELECT
+        @idAdministrador = idadmin,
+        @apeynom = ApeyNom,
+        @tel = tel,
+        @fechnac = fechnac,
+        @sexo = sexo,
+        @viveahi = viveahi,
+        @fechaModif = GETDATE(),
+        @usuario = CURRENT_USER,
+        @accion = 'accion modify'
+    FROM
+        inserted
+
+    INSERT INTO auditoria_administrador
+    VALUES (
+        @idAdministrador,
+        @apeynom,
+        @tel,
+        @fechnac,
+        @sexo,
+        @viveahi,
+        @fechaModif,
+        @usuario,
+        @accion
+    )
+END;
+
+	go
+CREATE TRIGGER trg_auditoria_administrador_eliminar
+ON administrador
+FOR delete
+AS
+BEGIN
+    DECLARE
+        @idAdministrador INT,
+        @apeynom VARCHAR(50),
+        @tel VARCHAR(50),
+        @fechnac DATE,
+        @sexo VARCHAR(1),
+        @viveahi VARCHAR(1),
+        @fechaModif DATE,
+        @usuario VARCHAR(50),
+        @accion VARCHAR(20)
+
+    SELECT
+        @idAdministrador = idadmin,
+        @apeynom = ApeyNom,
+        @tel = tel,
+        @fechnac = fechnac,
+        @sexo = sexo,
+        @viveahi = viveahi,
+        @fechaModif = GETDATE(),
+        @usuario = CURRENT_USER,
+        @accion = 'accion delete'
+    FROM
+        deleted
+
+    INSERT INTO auditoria_administrador
+    VALUES (
+        @idAdministrador,
+        @apeynom,
+        @tel,
+        @fechnac,
+        @sexo,
+        @viveahi,
+        @fechaModif,
+        @usuario,
+        @accion
+    )
+END;
+
+--
+--triggers
+--
+go
+	
+create login benn with password='Password123';
+create login artur with password='Password123';
+
+--creamos usuarios con los long in anteriores
+create user benn for login benn
+create user artur for login artur
+--asignamos roles a los usuarios
+alter role db_datareader add member benn
+alter role db_ddladmin add member artur
+
+go
+-- creamos el procedimiento insertarAdministrador
+create procedure insertarAdministrador
+@apeynom varchar(50),
+@viveahi varchar(1),
+@tel varchar(20),
+@s varchar(1),
+@nacimiento datetime
+as
+begin
+	insert into administrador(apeynom,viveahi,tel,sexo,fechnac)
+	values (@apeynom,@viveahi,@tel,@s,@nacimiento);
+end
+	
+
+GO
+Create User Tomas without login
+alter role [db_datareader] add member Tomas
+BEGIN TRANSACTION;
+BEGIN TRY  
+	   EXECUTE AS USER = 'Tomas'; 
+    exec insertarAdministrador 'nom ape1', 'S', '19870223', 'M','2003-05-26';
+      COMMIT;
+
+
+END TRY
+BEGIN CATCH
+    ROLLBACK;
+    PRINT 'Error en la transacción. Deshaciendo cambios.';
+END CATCH;
+REVERT;
+go
+
+grant execute on insertarAdministrador TO Tomas
+BEGIN TRANSACTION;
+BEGIN TRY
+	EXECUTE AS USER = 'Tomas';
+    exec insertarAdministrador 'nom ape1', 'S', '19870223', 'M','2003-05-26';
+       COMMIT;
+
+
+END TRY
+BEGIN CATCH
+    ROLLBACK;
+    PRINT 'Error en la transacción. Deshaciendo cambios.';
+END CATCH;
+REVERT;
+select* from auditoria_administrador
+
+-- scrit para hacer drop  y poder volver a ejecutar
+-- DROP TRIGGER trg_auditoria_administrador_insertar;
+-- DROP TRIGGER trg_auditoria_administrador_modificar;
+-- DROP TRIGGER trg_auditoria_administrador_eliminar;
+-- DROP TABLE auditoria_administrador;
+-- DROP LOGIN benn;
+-- DROP LOGIN artur;
+-- DROP USER benn;
+-- DROP USER artur;
+-- DROP USER Tomas;
+-- DROP PROCEDURE insertarAdministrador;
